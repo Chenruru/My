56574a505912c2f322e777688388c0c1c2ff334d
chenruru <18410120856@163.com>
2017-04-27 20:58:28 +0800

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package test.bwie.com.my_;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomViews mCustomView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCustomView = (CustomViews) findViewById(R.id.cv);
+        new Thread(mCustomView).start();
+    }
+}
